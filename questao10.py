f9987504ee6cad4096ac85322bb7760d30a53e2e
Izabella Jorge <elyeiza@hotmail.com>
2020-08-25 19:01:00 -0300

    Questão 10 terminada e modificada

---
+++ questao10.py
@@ -0,0 +1,9 @@
+num1 = int(input("Insira um número: "))
+num2 = int(input("Insira um número: "))
+while num2<num1:
+    num1 = int(input("Insira um número: "))
+    num2 = int(input("Insira um número: "))
+
+else:
+	for i in range(num1,num2,1):
+		print(i)
