ef10c6462be83738fa9ca3c678f11808d21d2919
Izabella Jorge <elyeiza@hotmail.com>
2020-08-25 19:33:08 -0300

    Questão 11 modificada e terminada

--- questao11.py
+++ questao11.py
@@ -1,2 +1,9 @@
-
+num1 = int(input("Insira o número inicial do intervalo: "))
+num2 = int(input("Insira o número inicial do intervalo: "))
+soma = 0
+while (num1<num2-1):
+    num1 = num1+1
+    print(num1)
+    soma = num1 + soma
     
+print("A soma dos intervalos é: ", soma)    
